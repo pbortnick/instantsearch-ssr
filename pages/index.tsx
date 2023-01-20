4d3974d8a597ad29bb5684a45e7b6698da1b6e5b
Pamela Bortnick <pbortnick@gmail.com>
2023-01-20 13:33:08 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
-import Head from "next/head";
-import React from "react";
-import { GetServerSideProps } from "next";
-import { renderToString } from "react-dom/server";
-import algoliasearch from "algoliasearch/lite";
-import { Hit as AlgoliaHit } from "instantsearch.js";
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import algoliasearch from 'algoliasearch/lite';
+import { Hit as AlgoliaHit } from 'instantsearch.js';
 import {
-  DynamicWidgets,
   InstantSearch,
   Hits,
   Highlight,
   RefinementList,
   SearchBox,
   InstantSearchServerState,
-  InstantSearchSSRProvider
-} from "react-instantsearch-hooks-web";
-import { getServerState } from "react-instantsearch-hooks-server";
-import { Panel } from "../components/Panel";
-import { createInstantSearchNextRouter } from "../utils/react-instantsearch-hooks-next-router";
+  InstantSearchSSRProvider,
+  DynamicWidgets,
+} from 'react-instantsearch-hooks-web';
+import { getServerState } from 'react-instantsearch-hooks-server';
+import { Panel } from '../components/Panel';
+import Link from 'next/link';
+import { renderToString } from 'react-dom/server';
+import { createInstantSearchNextRouter } from '../utils/react-instantsearch-hooks-next-router';
 
-const client = algoliasearch("latency", "6be0576ff61c053d5f9a3225e2a90f76");
+const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
 
 type HitProps = {
   hit: AlgoliaHit<{
@@ -29,10 +29,10 @@ type HitProps = {
 
 function Hit({ hit }: HitProps) {
   return (
-    <>
+    <Link href="/test" passHref>
       <Highlight hit={hit} attribute="name" className="Hit-label" />
       <span className="Hit-price">${hit.price}</span>
-    </>
+    </Link>
   );
 }
 
@@ -45,18 +45,24 @@ export default function HomePage({ serverState, url }: HomePageProps) {
   return (
     <InstantSearchSSRProvider {...serverState}>
       <Head>
-        <title>React InstantSearch Hooks - Next.js</title>
+        <title>React InstantSearch - Next.js</title>
       </Head>
 
       <InstantSearch
         searchClient={client}
         indexName="instant_search"
         routing={{
-          router: createInstantSearchNextRouter(url)
+          router: createInstantSearchNextRouter(url),
         }}
       >
         <div className="Container">
           <div>
+            <Panel header="brand">
+              <RefinementList attribute="brand" />
+            </Panel>
+            <Panel header="categories">
+              <RefinementList attribute="categories" />
+            </Panel>
             <DynamicWidgets fallbackComponent={FallbackComponent} />
           </div>
           <div>
@@ -77,19 +83,18 @@ function FallbackComponent({ attribute }: { attribute: string }) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps<HomePageProps> = async function getServerSideProps({
-  req
-}) {
-  const protocol = req.headers.referer?.split("://")[0] || "https";
-  const url = `${protocol}://${req.headers.host}${req.url}`;
-  const serverState = await getServerState(<HomePage url={url} />, {
-    renderToString
-  });
+export const getServerSideProps: GetServerSideProps<HomePageProps> =
+  async function getServerSideProps({ req }) {
+    const protocol = req.headers.referer?.split('://')[0] || 'https';
+    const url = `${protocol}://${req.headers.host}${req.url}`;
+    const serverState = await getServerState(<HomePage url={url} />, {
+      renderToString,
+    });
 
-  return {
-    props: {
-      serverState,
-      url
-    }
+    return {
+      props: {
+        serverState,
+        url,
+      },
+    };
   };
-};
